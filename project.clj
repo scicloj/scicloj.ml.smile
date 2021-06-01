264460d2b3c6b0ba6271356d4bbf7e148f8f022d
Carsten Behring <carsten.behring@gmail.com>
2021-06-01 09:37:33 +0200

    increased version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scicloj/scicloj.ml.smile "5.06"
+(defproject scicloj/scicloj.ml.smile "5.07-SNAPSHOT"
   :description "Smile models for scicloj.ml"
   :url "https://github.com/scicloj/scicloj.ml.smile"
   :license {:name "Eclipse Public License"
