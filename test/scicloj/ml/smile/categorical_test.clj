3e4f1abef32f099358252fde5aa07069159e5719
Carsten Behring <carsten.behring@gmail.com>
2021-09-24 23:19:14 +0200

    test for categorical

---
+++ test/scicloj/ml/smile/categorical_test.clj
@@ -0,0 +1,99 @@
+(ns scicloj.ml.smile.categorical-test
+  (:require  [clojure.test :as t]
+             [scicloj.metamorph.ml :as ml]
+             [tech.v3.dataset :as ds]
+             [tech.v3.dataset.modelling :as ds-mod]
+             [tech.v3.dataset.categorical :as ds-cat]
+
+             [scicloj.ml.smile.classification]))
+
+
+
+(def working-ds [(-> (ds/->dataset {:col-1 [:a :a :b :b
+                                            :a :a :b :b]
+                                    :y [1 2 1 2
+                                        2 1 2 1]})
+                     (ds-mod/set-inference-target :y)
+                     (ds/categorical->number [:col-1]))
+
+                 (-> (ds/->dataset {:col-1 [:a :a :b :b
+                                            :a :a :b :b]
+                                    :y [1 1 1 2
+                                        2 1 2 1]})
+                     (ds-mod/set-inference-target :y)
+                     (ds/categorical->number [:col-1]))
+
+                 (-> (ds/->dataset {:col-1 [:a :a :b :b
+                                            :a :a :b :b]
+                                    :y [:a :a :b :b
+                                        :a :b :b :b]})
+                     (ds-mod/set-inference-target :y)
+                     (ds/categorical->number [:col-1 :y]))])
+
+
+
+
+(t/deftest should-not-crash
+  (t/is (= 3
+           (->
+            (map
+             #(ml/train % {:model-type :smile.classification/decision-tree})
+             working-ds)
+            count))))
+
+
+
+(t/deftest float-target-should-crash
+  (try
+    (ml/train
+     (-> (ds/->dataset {:col-1 [:a :a :b :b
+                                :a :a :b :b]
+                        :y [1.1 1 1 0
+                            0 1 0 1]})
+         (ds-mod/set-inference-target :y)
+         (ds/categorical->number [:col-1]))
+     {:model-type :smile.classification/decision-tree})
+    (throw "failed")
+    (catch Exception e (t/is true))))
+
+(t/deftest rounded-float-target-should-crash
+  (try
+    (ml/train
+     (-> (ds/->dataset {:col-1 [:a :a :b :b
+                                :a :a :b :b]
+                        :y [1.0 1 1 0
+                            0 1 0 1]})
+         (ds-mod/set-inference-target :y)
+         (ds/categorical->number [:col-1]))
+     {:model-type :smile.classification/decision-tree})
+    (throw "failed")
+    (catch Exception e (t/is true))))
+
+(t/deftest keyword-target-should-crash
+  (try
+    (ml/train
+     (ml/train
+      (-> (ds/->dataset {:col-1 [:a :a :b :b
+                                 :a :a :b :b]
+                         :y [:a :a :b :b
+                             :a :b :b :b]})
+          (ds-mod/set-inference-target :y)
+          (ds/categorical->number [:col-1]))
+      {:model-type :smile.classification/decision-tree})
+
+     {:model-type :smile.classification/decision-tree})
+    (throw "failed")
+    (catch Exception e (t/is true))))
+
+
+
+(comment
+
+  (ml/thaw-model trained)
+
+  (->
+   (ml/predict (->  (ds/->dataset {:col-1 [:a]})
+                    (ds/categorical->number [:col-1]))
+               trained)
+   (ds/select-columns [:y])
+   (ds-cat/reverse-map-categorical-xforms)))
