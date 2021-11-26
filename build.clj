c47269cf15d6916b1e5e9b08b4f8db015bac1729
Carsten Behring <carsten.behring@gmail.com>
2021-11-26 23:58:27 +0100

    java 8 compatible

--- build.clj
+++ build.clj
@@ -15,8 +15,8 @@
 (defn compile [_]
   (b/javac {:src-dirs ["java"]
             :class-dir class-dir
-            :basis basis}))
-            ;; :javac-opts ["-source" "8" "-target" "8"]
+            :basis basis
+            :javac-opts ["-source" "8" "-target" "8"]}))
 
 
 
