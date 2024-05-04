6035bc5d90b6e8e10d2e3cc76c6567b99d057ecb
Carsten Behring <carsten.behring@gmail.com>
2024-05-04 11:49:21 +0200

    more robust test

--- test/scicloj/ml/smile/ols_test.clj
+++ test/scicloj/ml/smile/ols_test.clj
@@ -4,17 +4,13 @@
             [scicloj.ml.smile.regression]
             [scicloj.metamorph.ml :as ml]
             [scicloj.metamorph.ml.toydata]
+            [same.core :refer [ish?]]
             [clojure.test :refer (deftest is)]))
 
 (def interest-rate  [2.75 2.5 2.5 2.5 2.5 2.5 2.5 2.25 2.25 2.25 2 2 2 1.75 1.75 1.75 1.75 1.75 1.75 1.75 1.75 1.75 1.75 1.75])
 (def unemployment-rate [5.3 5.3 5.3 5.3 5.4 5.6 5.5 5.5 5.5 5.6 5.7 5.9 6 5.9 5.8 6.1 6.2 6.1 6.1 6.1 5.9 6.2 6.2 6.1])
 (def stock-index-price [1464 1394 1357 1293 1256 1254 1234 1195 1159 1167 1130 1075 1047 965 943 958 971 949 884 866 876 822 704 719])
 
-(defn absolute-difference ^double [^double x ^double y]
-  (Math/abs (double (- x y))))
-
-(defn close? [tolerance x y]
-  (< (absolute-difference x y) tolerance))
 
 
 (def ds (-> (ds/->dataset {:interest-rate interest-rate
@@ -37,30 +33,26 @@
 
         weights (ml/explain ols)]
 
-    (is (= [
+    (is (ish? [
 
-            {:adj.r.squared 0.8878844074567379
-             :mse 4356.611357123131
-             :rss 104558.67257095512
-             :p.value 4.042532223561903E-11
-             :df 21, :aic 277.2158453335483
-             :bic 281.9280606549401
-             :rmse 66.00463133086292
-             :r.squared 0.8976335894170215
-             :log-lik -134.60792266677416}]
+               {:adj.r.squared 0.8878844074567379
+                :mse 4356.611357123131
+                :rss 104558.67257095512
+                :p.value 4.042532223561903E-11
+                :df 21, :aic 277.2158453335483
+                :bic 281.9280606549401
+                :rmse 66.00463133086292
+                :r.squared 0.8976335894170215
+                :log-lik -134.60792266677416}]
 
            (ds/rows
             (ml/glance ols))))
 
 
-    (is (close? 0.1 -134.60792266677416 loglik))
-    (is (close? 0.1 1798.4  (:bias weights)))
-    (is (close? 0.1 345.5  (-> weights :coefficients first second)))
-    (is (close? 0.1 -250.1  (-> weights :coefficients second second)))))
-
-
-
-
+    (is (ish? -134.60792266677416 loglik))
+    (is (ish? 0.1 1798.4  (:bias weights)))
+    (is (ish? 0.1 345.5  (-> weights :coefficients first second)))
+    (is (ish? 0.1 -250.1  (-> weights :coefficients second second)))))
 
 (deftest tidy-fns
 
@@ -82,9 +74,6 @@
     (is (= [7 150]
            (ds/shape
             (ml/augment ols ds))))))
-
-
-
 
 
 (deftest fail-on-wrong-params
